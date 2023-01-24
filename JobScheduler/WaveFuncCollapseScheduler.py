0edda8afd7129cf6892cc7cc2e1ad9fa1b4245e8
DrSqua <dr.squa@gmail.com>
2023-01-24 18:33:39 +0100

    Switched to deepcopy for the WaveFuncCollapseScheduler

--- JobScheduler/WaveFuncCollapseScheduler.py
+++ JobScheduler/WaveFuncCollapseScheduler.py
@@ -2,7 +2,6 @@ from random import sample
 from copy import deepcopy
 
 from JobScheduler.JobSchedulingAgent import JobSchedulingAgent
-from Datatypes.Schedules.LinearSchedule import LinearSchedule
 
 
 class WaveFuncCollapseScheduler(JobSchedulingAgent):
@@ -11,34 +10,32 @@ class WaveFuncCollapseScheduler(JobSchedulingAgent):
 
     def fill_schedule(self, checkAvailability: bool = False):
         scheduleAvailabilityList: list = []
-        loopCount = 0
+        schedule = deepcopy(self.schedule)
 
-        while not self.schedule.is_filled():
-            loopCount += 1
-            print(f"Running loop {loopCount}")
-
-            for slotIndex, slot in enumerate(self.schedule.get_slotVector()):
+        while not schedule.is_filled():
+            for slotIndex, slot in enumerate(schedule.get_slotVector()):
+                # Check if slot has to be filled
                 if slot != -1:
                     scheduleAvailabilityList.append([])
                     continue
 
-                # add available Persons
+                # add available Persons for slot
                 slotAvailabilitList: list = []
-                startDate, endDate = self.schedule.get_timespan_dates(slotIndex)
-                for person in self.schedule.get_personVector():
+                startDate, endDate = schedule.get_timespan_dates(slotIndex)
+                for person in schedule.get_personVector():
                     if person.is_available(startDate, endDate):
-                        slotAvailabilitList.append(self.schedule.as_personIndex(person))
+                        slotAvailabilitList.append(schedule.as_personIndex(person))
                 scheduleAvailabilityList.append(slotAvailabilitList)
 
             # Get smallest list
             lenOptions = [len(personList) for personList in scheduleAvailabilityList]
-            lenOptions = list(map(lambda x: x if x!=0 else 9999, lenOptions))
+            lenOptions = list(map(lambda x: x if x != 0 else 9999, lenOptions))
             lowestOptions = min(lenOptions)
             slotWithLowestOptionsIndex = lenOptions.index(lowestOptions)
             listWithSmallestOptions = scheduleAvailabilityList[slotWithLowestOptionsIndex]
             choice = sample(listWithSmallestOptions, 1)[0]
-            self.schedule.set_person(slotIndex=slotWithLowestOptionsIndex, personIndex=choice)
+            schedule.set_person(slotIndex=slotWithLowestOptionsIndex, personIndex=choice)
             scheduleAvailabilityList.clear()
-        return self.schedule
+        return schedule
 
 
